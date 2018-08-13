648f4e5e63ce8de793382ba25a0b52900907af8c
Oscar Gonzalez <oscarg610@gmail.com>
2018-08-13 13:35:38 -0400

    fetch jobs from api - first pass

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 
 Vue.config.productionTip = false;
 
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    posts: {
+      fetching: true,
+      data: [],
+    },
+    cats: [],
+    userData: {},
   },
   mutations: {
-
+    fetchingPosts(state, payload: boolean) {
+      state.posts.fetching = payload;
+    },
+    setPosts(state, payload) {
+      state.posts.data = payload;
+    },
   },
   actions: {
+    fetchPosts({ commit }) {
+      commit('fetchingPosts', true);
 
+      return axios.get('jobs/latest').then((res) => {
+        commit('setPosts', res.data);
+        commit('fetchingPosts', false);
+        return res;
+      });
+    },
   },
 });
