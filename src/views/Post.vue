a01f980aea362f129793fd1f9041672f6882ec23
Oscar Gonzalez <oscarg610@gmail.com>
2018-08-18 15:10:01 -0400

    implement dynamic forms, from @oscar-g/job-board-schema

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,34 +1,31 @@
 <template lang="pug">
-  article.post
+  section.post-form
     h1 Post your job in minutes!
-    form
-      .form-group
-        b position:
-        |job title, tags, category, description
-      .form-group
-        b company:
-        div name,statement,logo,url,email
-        div more (bio, description)
-      .form-group
-        b upgrades
-        div highlight/color, logo, sticky
-    
-    h2 Step 2: Preview
-    h2 Step 3: Payment
+    div(v-for="(schema, name) in forms")
+      form(:id="name + 'Form'")
+        form-field(v-for="(field, key) in schema", :schema="field", :fieldName="key", :prefix="name")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// interface Field {
-//   name: string
-//   label: string
-//   type: 'text'|'select'|'checkbox'|'radio'|'textarea'
-//   value: string|number|string[]|number[]
-// };
+import { jobFormSchema, userFormSchema, companyFormSchema } from '@oscar-g/job-board-schema';
+import FormField from '../components/FormField.vue';
 
 @Component({
-  components: {
-  },
+  components: { FormField },
 })
-export default class Post extends Vue {}
+export default class Post extends Vue {
+  public forms = {
+    job: jobFormSchema.describe().children,
+    user: userFormSchema.describe().children,
+    comp: companyFormSchema.describe().children,
+  };
+}
 </script>
+
+<style lang="stylus" scoped>
+.post-form
+  max-width 768px
+  padding 1em
+</style>
+
