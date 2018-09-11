163f791b5a6f13a9455817f7a35bd9b7c007033a
Oscar Gonzalez <oscarg610@gmail.com>
2018-09-11 13:50:40 -0400

    feat(form): date input component
    #6

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -31,6 +31,10 @@
       .control.is-expanded
         div(:id='controlId')
         form-field-help(:formName="formName", :fieldName="fieldName")
+    .field(v-else-if="schema.type === 'date'")
+      .control.is-expanded
+        input.input(:name="fieldName" type="date", :id="controlId" v-model="model" v-on:focus="onFocus" v-on:blur="onBlur")
+      form-field-help(:formName="formName", :fieldName="fieldName")
     .field(v-else)
       .control.is-expanded
         input.input(:name="fieldName" type="text", :id="controlId" v-model="model" v-on:focus="onFocus" v-on:blur="onBlur")
